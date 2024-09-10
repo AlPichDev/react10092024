a3f602924297b277e6a9121b32b708dd9bf9d5cf
AlPichDev <103033133+AlPichDev@users.noreply.github.com>
2024-09-10 17:39:34 +0300

    upload our files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { App2 } from './App2'
+
+function App() {
+
+
+  return (
+    <p>
+      {1+2}
+    </p>
+
+  )
+}
+
+export default App
---
+++ src/App2.jsx
@@ -0,0 +1,10 @@
+export function App2(){
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo qui amet, nihil fugiat pariatur illo sit commodi esse rem repellendus suscipit quis, incidunt tempore beatae. Delectus perspiciatis alias rerum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo qui amet, nihil fugiat pariatur illo sit commodi esse rem repellendus suscipit quis, incidunt tempore beatae. Delectus perspiciatis alias rerum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo qui amet, nihil fugiat pariatur illo sit commodi esse rem repellendus suscipit quis, incidunt tempore beatae. Delectus perspiciatis alias rerum!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo qui amet, nihil fugiat pariatur illo sit commodi esse rem repellendus suscipit quis, incidunt tempore beatae. Delectus perspiciatis alias rerum!</p>
+        </>
+    )
+}
